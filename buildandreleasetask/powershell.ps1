519f588af404220ab2e5e51da67813a4347d20c3
MatthijsdeHeus <matthijs.heus@gmail.com>
2021-10-22 21:09:22 +0200

    PS Directory Exploration
    Getting more information about the data tree within the extension

--- buildandreleasetask/powershell.ps1
+++ buildandreleasetask/powershell.ps1
@@ -1,12 +1,47 @@
-[CmdletBinding()]
-param()
+function BuildTaxonomy {
+    [CmdletBinding()]
+    param()
 
-$root = Get-VstsInput -Name 'root'
-$taxonomyPath = Get-VstsInput -Name 'taxonomyPath'
-$headerStart = Get-VstsInput -Name 'headerStart'
-$headerEnd = Get-VstsInput -Name 'headerEnd'
+    $root = Get-VstsInput -Name 'root'
+    $taxonomyPath = Get-VstsInput -Name 'taxonomyPath'
+    $headerStart = Get-VstsInput -Name 'headerStart'
+    $headerEnd = Get-VstsInput -Name 'headerEnd'
 
-Write-Host "Path to the wiki: $root"
-Write-Host "Path to the taxonomy: $taxonomyPath"
-Write-Host "Beginning of the taxonomy header: $headerStart"
-Write-Host "Ending of the taxonomy header: $headerEnd"
+    $currentLocation = Get-Location
+    $dllLocation = Join-Path -Path $currentLocation -ChildPath "WikiTaxonomyGenerator\bin\Release\netcoreapp3.1\WikiTaxonomyGenerator.dll"
+
+    git init
+
+    $result = Get-ChildItem -Path "D:\a\1\s"
+    Write-Host "Result before deletion $result"
+
+    # Remove old taxonomy
+    Get-ChildItem -Path "D:\a\1\s\" -Directory -Filter $taxonomyPath | Remove-Item -Recurse -Confirm:$false -Force -Verbose
+
+    $result = Get-ChildItem -Path "D:\a\1\s"
+    Write-Host "Result after deletion $result"
+
+    # Create new taxonomy
+    Set-Location -Path "D:\a\1\s\"
+
+    dotnet $dllLocation --root $root --header-start $headerStart --header-end $headerEnd
+
+    $result = Get-ChildItem -Path "D:\a\1\s"
+    Write-Host "Result after generation $result"
+
+    Write-Host "-----------------------------------------------------------------------------"
+    Write-Host "View of the total directory"
+    Get-ChildItem -Path $dllLocation -Recurse
+    Write-Host "-----------------------------------------------------------------------------"
+
+    # Commit Changes
+    git status
+    git config user.name "Taxonomy Generator"
+    git config user.email "-"
+    git add Taxonomy
+    git commit -m "Generated Taxonomies"
+    git checkout -b markdown-extension-test
+    git push origin markdown-extension-test
+}
+
+BuildTaxonomy

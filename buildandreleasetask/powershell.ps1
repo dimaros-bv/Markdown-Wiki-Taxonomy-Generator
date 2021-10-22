b3094a324307bda48b4a0d53348ce93d3941f704
MatthijsdeHeus <matthijs.heus@gmail.com>
2021-10-22 19:13:53 +0200

    Organizing
    - Added icon for task
    - Removed build yaml for local pipeline
    - Task powershell integration test with task.json

--- buildandreleasetask/powershell.ps1
+++ buildandreleasetask/powershell.ps1
@@ -1,4 +1,12 @@
 [CmdletBinding()]
 param()
 
-Write-Host "PowerShell script test"
+$root = Get-VstsInput -Name 'root'
+$taxonomyPath = Get-VstsInput -Name 'taxonomyPath'
+$headerStart = Get-VstsInput -Name 'headerStart'
+$headerEnd = Get-VstsInput -Name 'headerEnd'
+
+Write-Host "Path to the wiki: $root"
+Write-Host "Path to the taxonomy: $taxonomyPath"
+Write-Host "Beginning of the taxonomy header: $headerStart"
+Write-Host "Ending of the taxonomy header: $headerEnd"

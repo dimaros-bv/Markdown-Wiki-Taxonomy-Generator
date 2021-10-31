256ef48b716c44535abe5f70239838ed91a738f9
MatthijsdeHeus <matthijs.heus@gmail.com>
2021-10-31 22:11:56 +0100

    Cleaned up powershell, added git test

--- buildandreleasetask/powershell.ps1
+++ buildandreleasetask/powershell.ps1
@@ -7,35 +7,23 @@ function BuildTaxonomy {
     $headerStart = Get-VstsInput -Name 'headerStart'
     $headerEnd = Get-VstsInput -Name 'headerEnd'
 
+    $branchName = "$(Build.SourceBranch)" -replace "refs/heads",""
+    Write-Host "Current branch name: $branchName"
+
     $currentLocation = Get-Location
+
     $dllLocation = Join-Path -Path $currentLocation -ChildPath "WikiTaxonomyGenerator\bin\Release\netcoreapp3.1\WikiTaxonomyGenerator.dll"
-    #$dllLocation = Join-Path -Path $currentLocation -ChildPath "WikiTaxonomyGenerator\bin\Release\netcoreapp3.1\wronglocation.dll"
 
     Set-Location -Path "D:\a\1\s"
     git init
 
-    # Actual path to the taxonomy
+    # Remove old taxonomy
     $pathToTaxonomy = Join-Path -Path "D:\a\1\s\" -ChildPath $taxonomyPath
 
-    $result = Get-ChildItem -Path $pathToTaxonomy
-    Write-Host "Result before deletion $result"
-
-    # Remove old taxonomy
     Get-ChildItem -Path $pathToTaxonomy | Remove-Item -Recurse -Confirm:$false -Force -Verbose
-
-    $result = Get-ChildItem -Path $pathToTaxonomy
-    Write-Host "Result after deletion $result"
 
     # Create new taxonomy
     dotnet $dllLocation --root "D:\a\1\s" --header-start $headerStart --header-end $headerEnd
-
-    $result = Get-ChildItem -Path $pathToTaxonomy
-    Write-Host "Result after generation $result"
-
-    Write-Host "-----------------------------------------------------------------------------"
-    Write-Host "View of the taxonomy directory"
-    Get-ChildItem -Path "D:\a\1\s\" -Recurse
-    Write-Host "-----------------------------------------------------------------------------"
 
     # Commit Changes
     git status
